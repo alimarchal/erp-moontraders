dda14c85781d182d41f120ab0b198990fb79f14f
Ali Raza Marchal <kh.marchal@gmail.com>
2026-02-19 11:36:27 +0500

    fix: add missing UserTracking trait to SalesSettlementCashDenomination model

--- app/Models/SalesSettlementCashDenomination.php
+++ app/Models/SalesSettlementCashDenomination.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\UserTracking;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class SalesSettlementCashDenomination extends Model
 {
     use HasFactory;
+    use UserTracking;
 
     protected $fillable = [
         'sales_settlement_id',

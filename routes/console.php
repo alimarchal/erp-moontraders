d65a1230bbae48a462fc126f91a8ed559e5fa536
Ali Raza Marchal <kh.marchal@gmail.com>
2026-04-05 15:15:09 +0500

    Add logging for schedule run in inspire command

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    Log::info('Schedule Run');
 })->purpose('Display an inspiring quote');
 
 // Take a daily end-of-day inventory snapshot at 11:59 PM
